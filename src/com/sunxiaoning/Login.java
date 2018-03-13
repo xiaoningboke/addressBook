2d17cf42aef31f25f389c09a1248eba235f4f98a
xiaoningboke <sunxiaoning@aliyun.com>
2018-03-13 10:50:10 +0800

    Initial commit

---
+++ src/com/sunxiaoning/Login.java
@@ -0,0 +1,18 @@
+package com.sunxiaoning;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame implements ActionListenernListener{
+    private JPanel jp = new JPanel();
+    private JLabel jname = new JLabel("用户名");
+    private JLabel jpwd = new JLabel("密码");
+    private JButton jdl = new JButton("登录");
+    private JButton jzc = new JButton("注册");
+    private JButton jdl = new JButton("登录");
+    private JButton jdl = new JButton("登录");
+
+    public static void main(String[] argc){
+
+    }
+}
